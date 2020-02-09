2366ac2fb33c993e3465d5390b6e725ab4e12b0f
Andrew Sackville-West <asackvil@amazon.com>
2020-02-09 12:22:30 -0800

    enforce update of requirements.txt on commit
    because I keep forgetting to do it...

--- bin/pre-commit-hook.sh
+++ bin/pre-commit-hook.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+# update coverage badge
 rm coverage.svg
 coverage-badge -o coverage.svg
-
 git add coverage.svg
+
+# ensure we commit requirements.txt routinely
+pip freeze > requirements.txt
+git add requirements.txt
